57b30cd8529d795fea39f020f66d0448b5045170
Emmar Caber <caberemmar@gmail.com>
2024-09-27 10:25:41 +0800

    Refactor user seeder to update default user credentials

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,13 +19,13 @@ class UserSeeder extends Seeder
             [
                 'name' => 'Admin',
                 'email' => 'admin@nackingspol.com',
-                'password' => bcrypt('nackingspol2022'),
+                'password' => "$2y$12\$c5Zfomk1whNFcD.OoahRA.KyOzAf5jSHRJFhZtDK3qivB/uYV3GLq",
                 'role' => RoleType::SUPER_ADMIN
             ],
             [
-                'name' => 'Mary Joy Bedico',
-                'email' => 'mjbedico@nackingspol.com',
-                'password' => bcrypt('mjbediconackingspol2022'),
+                'name' => 'Cashier',
+                'email' => 'cashier@nackingspol.com',
+                'password' => "$2y$12\$LFzhjv4DliJU2r7MeAK8CuM3HJDCW49jYrZpEeE/MvwjaQJr/mTUu",
                 'role' => RoleType::CASHIER,
             ]
         ];
