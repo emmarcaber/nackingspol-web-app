c392a2d92651224dce18349fa84bff09305b39cf
Emmar Caber <caberemmar@gmail.com>
2024-09-27 11:56:02 +0800

    feat: add Customer, Order and Product fillable attributes

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -12,6 +12,14 @@ class Order extends Model
     use HasFactory;
     use SoftDeletes;
 
+    protected $fillable = [
+        'cashier_id',
+        'customer_id',
+        'product_id',
+        'quantity',
+        'total',
+    ];
+
     public function cashier(): BelongsTo
     {
         return $this->belongsTo(User::class);
