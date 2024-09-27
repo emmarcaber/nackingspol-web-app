0cb1bba992b22733827a53dba6abb0705746a5da
Emmar Caber <caberemmar@gmail.com>
2024-09-27 14:39:58 +0800

    refactor: add permissions for cashier in PermissionSeeder

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -2,10 +2,15 @@
 
 namespace Database\Seeders;
 
+use App\Models\Size;
 use App\Models\User;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\Customer;
+use App\Models\WaterType;
 use Illuminate\Database\Seeder;
 use App\Helpers\RolePermissionFactoryHelper;
-use App\Models\WaterType;
+use App\Types\RoleType;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class PermissionSeeder extends Seeder
@@ -16,6 +21,55 @@ class PermissionSeeder extends Seeder
     public function run(): void
     {
         RolePermissionFactoryHelper::make(User::class);
-        RolePermissionFactoryHelper::make(WaterType::class);
+
+        RolePermissionFactoryHelper::make(WaterType::class)
+            ->addPermissions([
+                RoleType::CASHIER => [
+                    'view any',
+                    'view',
+                    'create',
+                    'update',
+                ]
+            ]);
+
+        RolePermissionFactoryHelper::make(Size::class)
+            ->addPermissions([
+                RoleType::CASHIER => [
+                    'view any',
+                    'view',
+                    'create',
+                    'update',
+                ]
+            ]);
+
+        RolePermissionFactoryHelper::make(Customer::class)
+            ->addPermissions([
+                RoleType::CASHIER => [
+                    'view any',
+                    'view',
+                    'create',
+                    'update',
+                ]
+            ]);
+
+        RolePermissionFactoryHelper::make(Order::class)
+            ->addPermissions([
+                RoleType::CASHIER => [
+                    'view any',
+                    'view',
+                    'create',
+                    'update',
+                ]
+            ]);
+
+        RolePermissionFactoryHelper::make(Product::class)
+            ->addPermissions([
+                RoleType::CASHIER => [
+                    'view any',
+                    'view',
+                    'create',
+                    'update',
+                ]
+            ]);
     }
 }
